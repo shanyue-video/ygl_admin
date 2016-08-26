c354e9d18f3b90e370e841ba2b96ba5293bf1b09
shanyu-e <554816284@qq.com>
2016-08-26 16:15:49 +0800

    ...

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 filename=$(date +%Y%m%d%H%M%S)
+server_path="/alidata/www/ygl/"
 meteor build --server-only --server="http://m.yigonglue.com" ~/workplace/meteor_output/${filename}
 cp main.json ~/workplace/meteor_output/${filename}/
 cp server_build.sh ~/workplace/meteor_output/${filename}/
 cd ~/workplace/meteor_output
-scp -r ${filename} yjl:/alidata/www/ygl/
-ssh yjl "cd /alidata/www/ygl/${filename};/bin/bash /alidata/www/ygl/${filename}/server_build.sh"
+scp -r ${filename} yjl:${server_path}
+ssh yjl "cd ${server_path}${filename};/bin/bash ${server_path}${filename}/server_build.sh"
--- server_build.sh
+++ server_build.sh
@@ -1,7 +1,11 @@
 tar zxvf ygl_admin.tar.gz
 cp main.json bundle/
 cd bundle/programs/server
-nvm use 4.4.7
+/root/.nvm/v0.10.45/bin/nvm use v4.4.7
 npm i
 cd ../../
-pm2 start main.json
+/root/.nvm/v0.10.45/bin/nvm use v4.4.7
+/root/.nvm/versions/node/v4.4.7/bin/pm2 stop admin2
+/root/.nvm/versions/node/v4.4.7/bin/pm2 start main.json
+/root/.nvm/versions/node/v4.4.7/bin/pm2 list
+/root/.nvm/versions/node/v4.4.7/bin/pm2 start admin2 &
