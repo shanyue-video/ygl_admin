dcdbe7fd0f8aaa775defc37035af0c2566e82312
shanyu-e <554816284@qq.com>
2016-10-17 18:43:11 +0800

    ...

--- client/modules/wx/components/chart.jsx
+++ client/modules/wx/components/chart.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
-import { Input } from 'antd';
+import { Input, Button } from 'antd';
 
 
 class Chart extends React.Component {
     constructor(props) {
         super(props);
         //console.log(props);
+        this.onClick = this.onClick.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onClick() {
+        //var inputValue = this.refs.["message"].getDOMNode().value;
+        //var inputValue = input.value;
+        console.log(this.state.message);
+    }
+
+    onChange(e) {
+        //var inputValue = this.refs.["message"].getDOMNode().value;
+        //var inputValue = input.value;
+        this.setState({message: e.target.value});
     }
 
     render() {
-        console.log(this.props);
+        //console.log(this.props);
+
+        const sendButton = (
+            <Button type="primary" onClick={this.onClick} size="small">发送</Button>
+        );
+
         return (
             <div>
                 <div className="login-header">
@@ -17,7 +36,10 @@ class Chart extends React.Component {
                 </div>
                 <div className="wx-page-login-body">
                 </div>
-                <Input style={{position: 'fixed', bottom: "0px"}} placeholder="输入聊天内容" />
+                <div style={{position: 'fixed', bottom: "0px"}}>
+                    <Input ref='message' placeholder="输入聊天内容" onChange={this.onChange}
+                           addonAfter={sendButton}/>
+                </div>
             </div>
         );
     }
