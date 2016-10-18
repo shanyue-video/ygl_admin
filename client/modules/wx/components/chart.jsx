b686be9d03263c3fd01ca5040e6d0df09463d118
shanyu-e <554816284@qq.com>
2016-10-18 12:31:55 +0800

    完成chart

--- client/modules/wx/components/chart.jsx
+++ client/modules/wx/components/chart.jsx
@@ -5,38 +5,53 @@ import { Input, Button } from 'antd';
 class Chart extends React.Component {
     constructor(props) {
         super(props);
-        //console.log(props);
         this.onClick = this.onClick.bind(this);
         this.onChange = this.onChange.bind(this);
     }
 
     onClick() {
-        //var inputValue = this.refs.["message"].getDOMNode().value;
-        //var inputValue = input.value;
         console.log(this.state.message);
         console.log(this.props.wx_chart_history._id);
         this.props.sendMessage(this.props.wx_chart_history._id, this.state.message);
     }
 
     onChange(e) {
-        //var inputValue = this.refs.["message"].getDOMNode().value;
-        //var inputValue = input.value;
         this.setState({message: e.target.value});
     }
 
     render() {
-        //console.log(this.props);
-
         const sendButton = (
             <Button type="primary" onClick={this.onClick} size="small">发送</Button>
         );
+
+        const chartMessages = [];
+        const messages = this.props.wx_chart_history ? this.props.wx_chart_history.messages: [];
+        for(let i of messages) {
+            //console.log(i);
+            let chartMessage;
+            if(i.from == 'user') {
+                chartMessage = (
+                    <div key={i.createAt.getTime()} style={{textAlign: "right"}}>
+                        {i.createAt.toLocaleString()} {i.message}：我
+                    </div>
+                );
+            } else if (i.from == 'doctor') {
+                chartMessage = (
+                    <div key={i.createAt.getTime()}>
+                        医生：{i.message} {i.createAt.toLocaleString()}
+                    </div>
+                );
+            }
+            chartMessages.push(chartMessage);
+        }
 
         return (
             <div>
                 <div className="login-header">
                     {this.props.wx_user ? this.props.wx_user.data.nickname: '未知'}医生
                 </div>
-                <div className="wx-page-login-body">
+                <div className="wx-chart-login-body">
+                    {chartMessages}
                 </div>
                 <div style={{position: 'fixed', bottom: "0px"}}>
                     <Input ref='message' placeholder="输入聊天内容" onChange={this.onChange}
