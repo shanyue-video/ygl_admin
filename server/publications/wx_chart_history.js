67cfdc95b1bb11d7815f40509168ff05b9acf194
shanyu-e <554816284@qq.com>
2016-11-10 22:40:54 +0800

    bug fix ..

--- server/publications/wx_chart_history.js
+++ server/publications/wx_chart_history.js
@@ -7,7 +7,10 @@ export default function () {
         // 发起聊天的都是患者
         if (role == 'user') {
             const wch = WxChartHistory.find({wx_user_id: wx_user_id, doctor_id: doctor_id}).fetch();
-            if (wch.length == 0) {
+            //console.log('!!!');
+            //console.log(doctor_id);
+            if (wch.length == 0 || !doctor_id) {
+            //if (wch.length == 0) {
                 const wx_user = WxUser.find({_id: wx_user_id}).fetch()[0];
                 const init_his = {
                     wx_user_id: wx_user._id,
--- server_build.sh
+++ server_build.sh
@@ -2,7 +2,7 @@ tar zxvf ygl_admin.tar.gz
 cp main.json bundle/
 cd bundle/programs/server
 /root/.nvm/v0.10.45/bin/nvm use v4.4.7
-npm i
+cnpm i
 cd ../../
 /root/.nvm/v0.10.45/bin/nvm use v4.4.7
 /root/.nvm/versions/node/v4.4.7/bin/pm2 delete admin2
