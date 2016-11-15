f53145ca140f35aad646d61bb9e807ec6c385686
shanyu-e <554816284@qq.com>
2016-11-15 22:25:32 +0800

    bug fix ..

--- server/publications/wx_chart_history.js
+++ server/publications/wx_chart_history.js
@@ -6,19 +6,18 @@ export default function () {
     Meteor.publish('wx_chart_history', function (wx_user_id, doctor_id, role) {
         // 发起聊天的都是患者
         if (role == 'user') {
-            const wch = WxChartHistory.find({wx_user_id: wx_user_id, doctor_id: doctor_id}).fetch();
-            if (wch.length == 0 && !doctor_id) {
+            if(!doctor_id) {
                 const wx_user = WxUser.find({u_id: wx_user_id}, {sort: {createAt: -1}}).fetch()[0];
                 doctor_id = wx_user.doctor_data.openid;
-                const init_his = {
-                    wx_user_id: wx_user.data.openid,
-                    doctor_id: wx_user.doctor_data.openid,
-                    init: true
-                };
-                WxChartHistory.insert(init_his);
-            } else if (wch.length >= 0 && !doctor_id) {
-                const wx_user = WxUser.find({u_id: wx_user_id}, {sort: {createAt: -1}}).fetch()[0];
-                doctor_id = wx_user.doctor_data.openid;
+                const wch = WxChartHistory.find({wx_user_id: wx_user_id, doctor_id: doctor_id}).fetch();
+                if(wch.length == 0) {
+                    const init_his = {
+                        wx_user_id: wx_user.data.openid,
+                        doctor_id: wx_user.doctor_data.openid,
+                        init: true
+                    };
+                    WxChartHistory.insert(init_his);
+                }
             }
             return WxChartHistory.find({wx_user_id: wx_user_id, doctor_id: doctor_id});
         } else if (role == 'doctor') {
